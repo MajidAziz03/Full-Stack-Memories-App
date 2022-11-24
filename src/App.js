673eb1404c485b12aa98c954430208ce0f6f78fd
Majid Aziz <azizmajid34@gmail.com>
2022-11-24 14:01:01 +0500

    created folders in client side and added css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import './App.css';
+import Form from './Components/Form/Form';
+import Posts from './Components/Posts/Posts.js';
 import Navbar from './Components/Navbar.js'
+import memories from './images/memories.png'
 
 function App() {
   return (
     <>
-    < Navbar /> 
+      < Navbar /> 
+      <div className="container">
+        <div className="card">
+          <div className="card-container">
+          <div><h1> Memories </h1></div>
+          <div><img className='img-thumbnail card-img' src={memories} alt="memoriespic" /></div>
+        </div>
+        </div>
+      </div>
+      <div className="container">
+        <div className="combine">
+          <div className="left-posts">
+            < Posts />
+          </div>
+          <div className="right-form">
+            < Form />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
 
 export default App;
+
+
---
+++ src/Components/Posts/Posts.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Post from './post/Post.js'
+
+
+const posts = () => {
+  return (
+      <>
+          <h1> Posts </h1>
+          < Post />
+          < Post />
+      </>
+  )
+}
+
+export default posts
